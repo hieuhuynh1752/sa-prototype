39988bd8a392feca00d53afdde1e0034674d53d7
Aksont <aastanivuk@gmail.com>
2024-01-26 15:38:16 +0200

    added websocket api

--- backend/HouseholdInfrastrucutreModule/DeviceOrchestrator/index.js
+++ backend/HouseholdInfrastrucutreModule/DeviceOrchestrator/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mqtt = require("mqtt");
 const app = express();
+const axios = require("axios");
 
 const broker = "mqtt://test.mosquitto.org";
 const topic = "/bet_system_device_orch";
@@ -16,6 +17,19 @@ app.post("/apply-decisions-to-devices", (req, res) => {
   console.log(topic);
 
   //TODO @Lorenzo send info about this through socket (like everything else)
+
+  try {
+    responseDecision = axios
+      .post("http://localhost:8121/send-message", { decisions }) // sending to WebSocket
+      .then((response) => {
+        return response.data;
+      })
+      .catch((error) => {
+        console.error("Error making API request:", error);
+      });
+  } catch (error) {
+    console.error("Error making API request:", error);
+  }
 
   res.status(200).send(topic);
 });
--- backend/WebSocketsModule/index.js
+++ backend/WebSocketsModule/index.js
@@ -6,33 +6,33 @@ const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-wss.on('connection', async function connection(ws) {
-  ws.on('message', function message(data) {
-    console.log('received: %s', data);
+wss.on("connection", async function connection(ws) {
+  ws.on("message", function message(data) {
+    console.log("received: %s", data);
   });
 
-  ws.send('test');
-  const dataFromDecisionMaking = await axios.get("http://localhost:8096/apply-decisions-to-devices");
-  ws.send(JSON.stringify(dataFromDecisionMaking))
+  ws.send("test");
 });
 
 function broadcastMessage(message) {
-  wss.clients.forEach((client)=>{
-    if (client.readyState === WebSocket.OPEN){
-      client.send(message)
+  console.log("trigered");
+  wss.clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(message);
     }
-  })
+  });
 }
-
-app.post('/send-message',(req, res)=>{
-  const message = req.body;
-  broadcastMessage(message)
-})
-
-exports.appfunc = app;
 
 // Start the server
 const PORT = process.env.PORT || 8099;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+app.post("/send-message", (req, res) => {
+  console.log("sendinnng");
+  const message = req.body;
+  broadcastMessage(message);
+});
+
+exports.appfunc = app;
