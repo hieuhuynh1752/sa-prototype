6078715c52bcd32743b798f0022aee36b546c3b6
Aksont <aastanivuk@gmail.com>
2024-01-26 17:05:45 +0200

    fixed import issues

--- backend/HouseholdInfrastrucutreModule/DeviceOrchestrator/index.js
+++ backend/HouseholdInfrastrucutreModule/DeviceOrchestrator/index.js
@@ -1,13 +1,12 @@
-import { sendLogsThroughWS, sendThroughWS } from "../../utils";
-
-import express from "express";
-import { connect } from "mqtt";
+const { sendLogsThroughWS, sendThroughWS } = require("../../utils");
+const express = require("express");
+const mqtt = require("mqtt");
 const app = express();
-import { post } from "axios";
+const axios = require("axios");
 
 const broker = "mqtt://test.mosquitto.org";
 const topic = "/bet_system_device_orch";
-const client = connect(broker);
+const client = mqtt.connect(broker);
 
 app.post("/apply-decisions-to-devices", async (req, res) => {
   const decisions = req.body;
@@ -18,40 +17,10 @@ app.post("/apply-decisions-to-devices", async (req, res) => {
   console.log("sent to:");
   console.log(topic);
 
-  // sending map data to WS
   sendThroughWS(decisions);
-  // try {
-  //   responseDecision = await post("http://localhost:8121/send-message", {
-  //     decisions,
-  //   }) // sending to WebSocket
-  //     .then((response) => {
-  //       return response.data;
-  //     })
-  //     .catch((error) => {
-  //       console.error("Error making API request:", error);
-  //     });
-  // } catch (error) {
-  //   console.error("Error making API request:", error);
-  // }
-
-  // sending logs
   sendLogsThroughWS("new instructions were sent");
-  // try {
-  //   responseDecision = await post(
-  //     "http://localhost:8121/send-message",
-  //     "new instructions were sent"
-  //   ) // sending to WebSocket
-  //     .then((response) => {
-  //       return response.data;
-  //     })
-  //     .catch((error) => {
-  //       console.error("Error making API request:", error);
-  //     });
-  // } catch (error) {
-  //   console.error("Error making API request:", error);
-  // }
 
   res.status(200).send(topic);
 });
 
-export const appfunc = app;
+exports.appfunc = app;
